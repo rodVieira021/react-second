a051cffe2b4645930f2b83083e94abcdc1d0fead
rodVieira021 <rodvieira021@gmail.com>
2022-04-24 04:58:29 +0300

    nothing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <h1>Hello, REACT</h1>, document.getElementById("root")
-)
+ReactDOM.render(<h1>Hello, REACT</h1>, document.getElementById('root'));
